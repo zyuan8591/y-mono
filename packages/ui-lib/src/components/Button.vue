68caf006197cab0e38f02236e6d1da5f405cd5cf
aaron <88093376+zyuan8591@users.noreply.github.com>
2025-04-17 00:53:43 +0800

    chore: project init

---
+++ packages/ui-lib/src/components/Button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+    label: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <button>
+        {{ props.label }}
+    </button>
+</template>
